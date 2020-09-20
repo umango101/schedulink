50fb16c6c9df2e93b45135e3eeac91b2ad5648d8
pocato3rd <robertcato01@gmail.com>
2020-09-20 01:10:34 -0500

    adding save/load section list

--- section.js
+++ section.js
@@ -28,22 +28,43 @@ class Section {
     }
 
     set name(name) {
-        return this.name = name;
+        this.name = name;
     }
 
     set day(day) {
-        return this.day = day;
+        this.day = day;
     }
 
     set time_start(time_start) {
-        return this.time_start = time_start;
+        this.time_start = time_start;
     }
 
     set time_end(time_end) {
-        return this.time_end = time_end;
+        this.time_end = time_end;
     }
 
     set link(link) {
-        return this.link = link;
+        this.link = link;
     }
+}
+
+// saving objects
+var testSect1 = new Section("16.001",1,10,0,11,0,"fake_link");
+var testSect2 = new Section("5.111",2, 10,30,12,0,"fake_link");
+
+var sects = [testSect1, testSect2];
+var testWeek = [[],[testSect1],[testSect2],[],[],[],[]];
+
+
+function saveSectionsList(sections) {
+    var JSONsects = JSON.stringify(sections);
+    chrome.storage.sync.set({sections: JSONsects}, function() {console.log("Sections list saved");});
+}
+
+function loadSectionsList() {
+    sections = []
+    chrome.storage.sync.get('sections', function(data) {
+        sections = JSON.parse(data.sections);
+    });
+    return sections;
 }
