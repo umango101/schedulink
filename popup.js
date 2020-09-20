64313a80c0f701be7b516ffa26e1a1c4962ea548
pocato3rd <robertcato01@gmail.com>
2020-09-19 19:41:00 -0500

    saving time to global storage
    day_idx = day of week index, Sun=0
    time is in user's timezone

--- popup.js
+++ popup.js
@@ -22,12 +22,14 @@ function getDTime() {
     var day_arr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
 
     var date = new Date();
-    var day_wk = date.getDay();
+    var day_idx = date.getDay();
     var current_hour = date.getHours();
     var current_mins = date.getMinutes();
 
+    chrome.storage.sync.set({day_idx: day_idx, current_hr: current_hour, current_min: current_mins}, function() { console.log("Day-time saved."); });
+    
     let dateTime = document.createElement('p');
-    dateTime.textContent = day_arr[day_wk]+' '+current_hour.toString()+":"+current_mins.toString();
+    dateTime.textContent = day_arr[day_idx]+' '+current_hour.toString()+":"+current_mins.toString();
     
     timeDiv.appendChild(dateTime);
 }
